2ade92fdd06089b0cd82f7f4e865743355fbdfd0
rodolfo dalla costa <rodolfo.dalla.costa@hotmail.com>
2020-10-16 01:06:18 -0300

    resolve conflitps

--- controllers/collectionPointController.js
+++ controllers/collectionPointController.js
@@ -5,29 +5,39 @@ exports.get_all_pontos_de_coleta = (req, res) => {
 };
 
 exports.get_ponto_de_coleta_by_id = async (req, res) => {
-    var ponto_de_coleta = await collectionPointsDao.pontos_de_coleta.find(ponto => ponto.id === req.params.id);
+    var ponto_de_coleta = await collectionPointsDao.pontos_de_coleta.filter(ponto => ponto.id === req.params.id);
     res.json(ponto_de_coleta);
 }
 
-exports.get_ponto_de_coleta_by_nome = async nome => {
-    var pontos_de_coleta = await collectionPointsDao.pontos_de_coleta.filter(ponto => ponto.nome === nome);
-    res.json(pontos_de_coleta);
-}
-
-exports.get_ponto_de_coleta_by_municipio_nome = async (req, res) => {
-    var pontos_de_coleta = await collectionPointsDao.pontos_de_coleta
-                            .filter(ponto => ponto.endereco.municipio === req.params.nome);
-    res.json(pontos_de_coleta);
-}
-
 exports.get_pontos_by_ibge = async (req, res) => {
-    var pontos_de_coleta = await collectionPointsDao.pontos_de_coleta.filter(o => o.endereco.ibge === parseInt(req.params.ibge))
+    var pontos_de_coleta = await collectionPointsDao.pontos_de_coleta
+                                    .filter(o => { return o.endereco.ibge === parseInt(req.params.ibge)});
     res.json(pontos_de_coleta);
 }
 
 exports.get_pontos_by_ibge_cep = async (req, res) => {
     var pontos_de_coleta = await collectionPointsDao.pontos_de_coleta
-                                    .filter(ponto => ponto.endereco.ibge === req.params.ibge 
-                                        && ponto.endereco.cep === req.params.cep);
+                                    .filter(ponto => { return ponto.endereco.ibge === parseInt(req.params.ibge) 
+                                        && ponto.endereco.cep === req.params.cep });
     res.json(pontos_de_coleta);
+}
+
+exports.get_pontos_by_ibge_material= async (req, res) => {
+    var pontos_de_coleta = await collectionPointsDao.pontos_de_coleta
+                                    .filter(ponto => { return ponto.endereco.ibge === parseInt(req.params.ibge)
+                                        && ponto.tipoMaterial.some(o => o.id === req.params.idMaterial)});
+    res.json(pontos_de_coleta);
+}
+
+exports.create_ponto_de_coleta = async (req, res) => {
+    var fake_id = Math.floor(Math.random() * 10000) % 9999;
+    res.json({"id": fake_id});
+}
+
+exports.update_ponto_de_coleta = (req, res) => {
+    res.json(collectionPointsDao.pontos_de_coleta[0]);
+}
+
+exports.delete_ponto_de_coleta = (req, res) => {
+    res.status(200).json({"message": "Successfully deleted"});
 }
--- routes/recolheecoRoutes.js
+++ routes/recolheecoRoutes.js
@@ -2,8 +2,13 @@ var collectionPoints = require('../controllers/collectionPointController');
 
 module.exports = (router) => {
 
+    router.post('/pontos-de-coleta', collectionPoints.create_ponto_de_coleta);
     router.get('/pontos-de-coleta', collectionPoints.get_all_pontos_de_coleta);
-    router.get('/pontos-de-coleta/:id', collectionPoints.get_all_pontos_de_coleta);
+    router.get('/pontos-de-coleta/:id', collectionPoints.get_ponto_de_coleta_by_id);
+    router.put('/pontos-de-coleta/:id', collectionPoints.update_ponto_de_coleta);
+    router.delete('/pontos-de-coleta/:id', collectionPoints.delete_ponto_de_coleta);
     router.get('/pontos-de-coleta/municipio/:ibge', collectionPoints.get_pontos_by_ibge);
     router.get('/pontos-de-coleta/municipio/:ibge/cep/:cep', collectionPoints.get_pontos_by_ibge_cep);
+    router.get('/pontos-de-coleta/municipio/:ibge/material/:idMaterial', collectionPoints.get_pontos_by_ibge_material);
+    router.get('/pontos-de-coleta/municipio/:ibge/material/:idMaterial', collectionPoints.get_pontos_by_ibge_material);
 }
